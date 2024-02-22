f43531cf694ea247db541c8fb75c665bc4e1bcfc
Peter Goodey <93730100+petergoodey@users.noreply.github.com>
2024-02-22 15:35:12 +0000

    Delete build-images.cmd

--- Runtimes/Debian/12/build-images.cmd
+++
@@ -1,4 +0,0 @@
-docker build . --file mcr-aspnet-6.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:6.0-bookworm-slim
-docker build . --file mcr-aspnet-7.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:7.0-bookworm-slim
-docker build . --file mcr-aspnet-8.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:8.0-bookworm-slim
-
