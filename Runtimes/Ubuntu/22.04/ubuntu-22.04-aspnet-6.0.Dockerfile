1c74f36fceec784d80d1513b7843888707c015c0
Peter Goodey <93730100+petergoodey@users.noreply.github.com>
2024-02-22 15:27:16 +0000

    Cleanup and add new docker images

--- Runtimes/Ubuntu/22.04/ubuntu-22.04-aspnet-6.0.Dockerfile
+++ Runtimes/Ubuntu/22.04/ubuntu-22.04-aspnet-6.0.Dockerfile
@@ -1,9 +1,3 @@
 # docker build . --file ubuntu-22.04-aspnet-6.0.Dockerfile -t abcpdf/ubuntu-22.04-aspnet:6.0
-FROM ubuntu:22.04
-ARG DEBIAN_FRONTEND=noninteractive
-RUN apt-get update && apt-get install -y ca-certificates aspnetcore-runtime-6.0
-# Libraries required for ABCpdf native linux components
-RUN apt-get install -y libx11-6 libgtk-3-0 libnss3 libdrm2 libgbm1 libasound2 libegl1
-# Install a reasonable set of fonts
-RUN apt-get install -y fonts-noto-core fonts-noto-mono fonts-noto-color-emoji
-RUN fc-cache -f -v
+FROM abcpdf/ubuntu-22.04-abcpdf-runtime-deps:latest
+RUN apt-get update && apt-get install -y aspnetcore-runtime-6.0
---
+++ build-images.cmd
@@ -0,0 +1,10 @@
+docker build . --file Runtimes/Ubuntu/22.04/ubuntu-22.04-abcpdf-runtime-deps.Dockerfile -t abcpdf/ubuntu-22.04-abcpdf-runtime-deps:latest
+docker build . --file Runtimes/Ubuntu/22.04/ubuntu-22.04-aspnet-6.0.Dockerfile -t abcpdf/ubuntu-22.04-aspnet:6.0
+docker build . --file Runtimes/Ubuntu/22.04/ubuntu-22.04-aspnet-7.0.Dockerfile -t abcpdf/ubuntu-22.04-aspnet:7.0
+docker build . --file Runtimes/Ubuntu/22.04/ubuntu-22.04-aspnet-8.0.Dockerfile -t abcpdf/ubuntu-22.04-aspnet:8.0
+docker build . --file Runtimes/Ubuntu/22.04/mcr-aspnet-6.0-jammy.Dockerfile -t abcpdf/mcr-aspnet:6.0-jammy
+docker build . --file Runtimes/Ubuntu/22.04/mcr-aspnet-7.0-jammy.Dockerfile -t abcpdf/mcr-aspnet:7.0-jammy
+docker build . --file Runtimes/Ubuntu/22.04/mcr-aspnet-8.0-jammy.Dockerfile -t abcpdf/mcr-aspnet:8.0-jammy
+docker build . --file Runtimes/Debian/12/mcr-aspnet-6.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:6.0-bookworm-slim
+docker build . --file Runtimes/Debian/12/mcr-aspnet-7.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:7.0-bookworm-slim
+docker build . --file Runtimes/Debian/12/mcr-aspnet-8.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:8.0-bookworm-slim
