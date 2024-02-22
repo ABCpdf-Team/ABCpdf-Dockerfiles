d6ff21ecb572ff5b392f4914d628c60756db828b
Peter Goodey <93730100+petergoodey@users.noreply.github.com>
2024-02-22 15:27:16 +0000

    Cleanup and add new docker images

---
+++ Runtimes/Debian/12/build-images.cmd
@@ -0,0 +1,4 @@
+docker build . --file mcr-aspnet-6.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:6.0-bookworm-slim
+docker build . --file mcr-aspnet-7.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:7.0-bookworm-slim
+docker build . --file mcr-aspnet-8.0-bookworm-slim.Dockerfile -t abcpdf/mcr-aspnet:8.0-bookworm-slim
+
