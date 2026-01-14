13cae6cd09333155bd0dcd5ceec9586af6199ba8
Peter Goodey <goodey.peter@gmail.com>
2026-01-14 18:29:11 +0000

    Workflow online push test

--- Testing/IntegrationTests/TestContainer.cs
+++ Testing/IntegrationTests/TestContainer.cs
@@ -10,21 +10,23 @@ public sealed class TestContainer
 
 	private const string ImageName = "abcpdf-testcontainer";
 	private IContainer? _container;
-		
+	// This is the image tag of the abcpdf runtime docker image under test: e.g. " abcpdf/mcr-aspnet:10.0"
+	// It is expected to be set as an environment variable in the test runner environment.
+	private readonly string _baseImageRc = GetEnvVar("BASE_IMAGE_RC");
+	private readonly string _abcpdfLicense = GetEnvVar("ABCPDF_LICENSE_KEY");
 	public async Task StartAsync(CancellationToken cancellationToken = default)
 	{
-		var baseImageRc = Environment.GetEnvironmentVariable("BASE_IMAGE_RC");
 		var loggerFactory = LoggerFactory.Create(builder =>
 		{
 			builder
 				.SetMinimumLevel(LogLevel.Debug)
 				.AddConsole();
 		});
-		var license = Environment.GetEnvironmentVariable("ABCPDF_LICENSE_KEY");
+
 		var appDir = Path.Combine(CommonDirectoryPath.GetProjectDirectory().DirectoryPath);
 		var image = new ImageFromDockerfileBuilder()
 			.WithBuildArgument("RESOURCE_REAPER_SESSION_ID", ResourceReaper.DefaultSessionId.ToString("D"))
-			.WithBuildArgument("BASE_IMAGE_RC", baseImageRc)
+			.WithBuildArgument("BASE_IMAGE_RC", _baseImageRc)
 			.WithBuildArgument("BUILD_CONFIGURATION", "Release")
 			.WithDockerfileDirectory(appDir)
 			.WithContextDirectory(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath)
@@ -35,9 +37,10 @@ public sealed class TestContainer
 		await image.CreateAsync(cancellationToken).ConfigureAwait(false);
 		var network = new NetworkBuilder().Build();
 			
-		using IOutputConsumer outputConsumer = Consume.RedirectStdoutAndStderrToConsole();
+		using var outputConsumer = Consume.RedirectStdoutAndStderrToConsole();
 
-		_container = new ContainerBuilder(ImageName)
+		_container = new ContainerBuilder(image.FullName)
+			.WithName("abcpdf_test_app_container")
 			.WithOutputConsumer(outputConsumer)
 			.WithWaitStrategy(
 				Wait.ForUnixContainer()
@@ -47,9 +50,14 @@ public sealed class TestContainer
 			.WithNetwork(network)
 			.WithPortBinding(8080, true)
 			.WithPortBinding(8081, true)
-			.WithEnvironment("ABCPDF_LICENSE_KEY", license)
+			.WithEnvironment("ABCPDF_LICENSE_KEY", _abcpdfLicense)
 			.Build();
+
 		await _container.StartAsync(cancellationToken).ConfigureAwait(false);
+		if (_container.State != TestcontainersStates.Running)
+		{
+			throw new ApplicationException("Failed to start test application container.");
+		}
 	}
 	public Uri ConstructUri(string subPath)
 	{
@@ -60,4 +68,10 @@ public sealed class TestContainer
 	}
 
 	public string GetTestFilesFolder() => Path.Combine(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath, "TestFiles");
+
+	private static string GetEnvVar(string EnvVarName)
+	{
+		var value = Environment.GetEnvironmentVariable(EnvVarName);
+		return string.IsNullOrWhiteSpace(value) ? throw new InvalidOperationException($"{EnvVarName} env var must be set.") : value;
+	}
 }
