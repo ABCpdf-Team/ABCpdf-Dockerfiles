c571e94b3d44649515152a2394f2c0bbdc44341c
Peter Goodey <goodey.peter@gmail.com>
2026-01-16 00:16:36 +0000

    Fix test container env vars

--- Testing/IntegrationTests/TestContainer.cs
+++ Testing/IntegrationTests/TestContainer.cs
@@ -9,17 +9,15 @@ public sealed class TestContainer
 {
 	// This is the image tag of the abcpdf runtime docker image under test: e.g. " abcpdf/mcr-aspnet:10.0"
 	// It is expected to be set as an environment variable in the test runner environment.
-	private readonly string _baseImageRc = GetEnvVar("BASE_IMAGE_TAG");
+	private readonly string _testAppImageTag = GetEnvVar("TEST_APP_IMAGE_TAG");
 	private readonly string _abcpdfLicense = GetEnvVar("ABCPDF_LICENSE_KEY");
-	private readonly string _dotnetVersion = GetEnvVar("DOTNET_VERSION");
 	private IContainer? _container;
 
 	public async Task StartAsync(CancellationToken cancellationToken = default)
 	{
-		var image = await BuildAsync(cancellationToken);
 		using var outputConsumer = Consume.RedirectStdoutAndStderrToConsole();
 
-		_container = new ContainerBuilder(image.FullName)
+		_container = new ContainerBuilder(_testAppImageTag)
 			.WithName("abcpdf_test_app_container")
 			.WithOutputConsumer(outputConsumer)
 			.WithWaitStrategy(
@@ -31,7 +29,6 @@ public sealed class TestContainer
 			.WithPortBinding(8080, true)
 			.WithPortBinding(8081, true)
 			.WithEnvironment("ABCPDF_LICENSE_KEY", _abcpdfLicense)
-			.WithEnvironment("DOTNET_VERSION", _dotnetVersion)
 			.Build();
 
 		await _container.StartAsync(cancellationToken).ConfigureAwait(false);
@@ -39,33 +36,6 @@ public sealed class TestContainer
 		{
 			throw new ApplicationException("Failed to start test application container.");
 		}
-	}
-
-	private async Task<IFutureDockerImage> BuildAsync(CancellationToken cancellationToken)
-	{
-		var loggerFactory = LoggerFactory.Create(builder =>
-		{
-			builder
-				.SetMinimumLevel(LogLevel.Debug)
-				.AddConsole();
-		});
-
-		var imageName = $"abcpdf-testcontainer{_dotnetVersion}";
-
-		var appDir = Path.Combine(CommonDirectoryPath.GetProjectDirectory().DirectoryPath);
-		var image = new ImageFromDockerfileBuilder()
-			.WithBuildArgument("RESOURCE_REAPER_SESSION_ID", ResourceReaper.DefaultSessionId.ToString("D"))
-			.WithBuildArgument("TARGET_FRAMEWORK", $"net{_dotnetVersion}")
-			.WithBuildArgument("BASE_IMAGE_RC", _baseImageRc)
-			.WithBuildArgument("BUILD_CONFIGURATION", "Release")
-			.WithDockerfileDirectory(appDir)
-			.WithContextDirectory(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath)
-			.WithName(imageName)
-			.WithLogger(loggerFactory.CreateLogger("ImageFromDockerfileBuilder"))
-			.WithCleanUp(false)
-			.Build();
-		await image.CreateAsync(cancellationToken).ConfigureAwait(false);
-		return image;
 	}
 
 	public Uri ConstructUri(string subPath)
