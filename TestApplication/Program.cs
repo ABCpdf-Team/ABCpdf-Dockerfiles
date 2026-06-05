828a4a421154c1163e07cb6864521802237a9558
Peter Goodey <93730100+petergoodey@users.noreply.github.com>
2026-06-06 00:12:54 +0100

    Cleanup

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -6,7 +6,7 @@ using WebSupergoo.ABCpdf14;
 
 Console.WriteLine($"Starting PDF generation test application on .NET {Environment.OSVersion}...");
 Console.WriteLine($".NET version: {Environment.Version}");
-Console.WriteLine($"Current ABCpdf version: {XSettings.Version}");
+Console.WriteLine($"ABCpdf version: {XSettings.Version}");
 
 var abcPdfLicense = Environment.GetEnvironmentVariable("ABCPDF_LICENSE_KEY") ??
 	throw new InvalidOperationException("ABCpdf license key is not configured. Please set the license key in the 'ABCPDF_LICENSE_KEY' environment variable or user-secret.");
@@ -20,7 +20,7 @@ const string htmlFilePath = "simple.html";
 try{
     using Doc doc = new();
     doc.HtmlOptions.Engine = EngineType.Chrome123;
-    Console.WriteLine($"Current ABCChrome version: {doc.HtmlOptions.Engine}");
+    Console.WriteLine($"Using ABCChrome version: {doc.HtmlOptions.Engine}");
     doc.AddImageHtml(await new StreamReader(htmlFilePath).ReadToEndAsync());
     doc.Save("output.pdf");
 }
---
+++ run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SECRETS_FILE=".secrets"
+if [ -f "$SECRETS_FILE" ]; then
+    export $(grep -v '^#' "$SECRETS_FILE" | xargs)
+else
+    echo "Error: $SECRETS_FILE file not found!"
+    exit 1
+fi
+
+#./.github/scripts/build-and-test-distro.sh bookworm-slim 8.0 13 123 ${ABCPDF13_LICENSE_KEY}
+#./.github/scripts/build-and-test-distro.sh 22.04-jammy 8.0 13 123 ${ABCPDF13_LICENSE_KEY}
+#./.github/scripts/build-and-test-distro.sh 24.04-noble 10.0 13 123 ${ABCPDF13_LICENSE_KEY}
+#./.github/scripts/build-and-test-distro.sh 26.04-resolute 10.0 14 123 ${ABCPDF14_LICENSE_KEY}
+#./.github/scripts/build-and-test-distro.sh 26.04-resolute 10.0 14 123 ${ABCPDF14_LICENSE_KEY}
+exit $?
